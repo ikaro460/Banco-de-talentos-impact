c291c9c6a759335bffb2c8460380222e1b5fb472
Ícaro Nascimento <ikaro_460@hotmail.com>
2023-01-12 04:13:42 -0300

    feat!: show more button

--- swapi-frontend-test/src/providers/Swapi/index.jsx
+++ swapi-frontend-test/src/providers/Swapi/index.jsx
@@ -7,28 +7,36 @@ export const SwapiProvider = ({ children }) => {
   const [people, setPeople] = useState([]);
   const [films, setFilms] = useState([]);
   const [loading, setLoading] = useState(true);
+  const [nextPage, setNextPage] = useState("");
 
-  const getSwapi = () => {
-    api.get(`people/?page=1`).then((res) => {
-      setPeople(res);
-    });
-
-    api.get(`films`).then((res) => {
-      setFilms(res);
+  async function getPeople() {
+    await api.get(`people/?page=1`).then((res) => {
+      setPeople(res.data.results);
+      setNextPage(res.data.next);
       setLoading(false);
     });
-  };
+  }
+
+  async function getFilms() {
+    await api.get(`films`).then((res) => {
+      setFilms([...films, ...res.data.results]);
+      setLoading(false);
+    });
+  }
 
   return (
     <SwapiContext.Provider
       value={{
         people,
         setPeople,
+        nextPage,
+        setNextPage,
         films,
         setFilms,
         loading,
         setLoading,
-        getSwapi,
+        getPeople,
+        getFilms,
       }}
     >
       {children}
